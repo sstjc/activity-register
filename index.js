79622a6c1dda9cab7241b4dd863b282a0cc273b1
JimmyMa <jimmatw@gmail.com>
2021-03-31 14:24:36 +0800

    init form to react style

--- index.js
+++ index.js
@@ -8,38 +8,182 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var RollCall = function (_React$Component) {
-  _inherits(RollCall, _React$Component);
+var RollCallForm = function (_React$Component) {
+    _inherits(RollCallForm, _React$Component);
 
-  function RollCall(props) {
-    _classCallCheck(this, RollCall);
+    function RollCallForm() {
+        _classCallCheck(this, RollCallForm);
 
-    var _this = _possibleConstructorReturn(this, (RollCall.__proto__ || Object.getPrototypeOf(RollCall)).call(this, props));
-
-    _this.state = { liked: false };
-    return _this;
-  }
-
-  _createClass(RollCall, [{
-    key: 'render',
-    value: function render() {
-      var _this2 = this;
-
-      if (this.state.liked) {
-        return 'You liked this.';
-      }
-
-      return React.createElement(
-        'button',
-        { onClick: function onClick() {
-            return _this2.setState({ liked: true });
-          } },
-        'Like'
-      );
+        return _possibleConstructorReturn(this, (RollCallForm.__proto__ || Object.getPrototypeOf(RollCallForm)).apply(this, arguments));
     }
-  }]);
 
-  return RollCall;
+    _createClass(RollCallForm, [{
+        key: "render",
+        value: function render() {
+            return React.createElement(
+                "div",
+                null,
+                React.createElement(
+                    "form",
+                    { action: "#" },
+                    React.createElement(
+                        "div",
+                        { "class": "container" },
+                        React.createElement(
+                            "h1",
+                            null,
+                            "Roll Call"
+                        ),
+                        React.createElement(
+                            "div",
+                            null,
+                            "\u662F\u5426\u6709\u4FE1\u5F92\u7DE8\u865F: ",
+                            React.createElement("input", { type: "checkbox", name: "check_member" })
+                        ),
+                        React.createElement(
+                            "div",
+                            null,
+                            React.createElement(
+                                "label",
+                                { "for": "member_id" },
+                                React.createElement(
+                                    "b",
+                                    null,
+                                    "\u7DE8\u865F"
+                                )
+                            ),
+                            React.createElement("input", { type: "text", placeholder: "\u8ACB\u8F38\u5165", name: "member_id", required: true })
+                        ),
+                        React.createElement(
+                            "div",
+                            null,
+                            React.createElement(
+                                "label",
+                                { "for": "name" },
+                                React.createElement(
+                                    "b",
+                                    null,
+                                    "\u59D3\u540D"
+                                )
+                            ),
+                            React.createElement("input", { type: "text", placeholder: "\u8ACB\u8F38\u5165", name: "name", required: true })
+                        ),
+                        React.createElement(
+                            "div",
+                            null,
+                            React.createElement(
+                                "label",
+                                { "for": "phone" },
+                                React.createElement(
+                                    "b",
+                                    null,
+                                    "\u96FB\u8A71\u865F\u78BC"
+                                )
+                            ),
+                            React.createElement("input", { type: "text", placeholder: "\u8ACB\u8F38\u5165", name: "phone", required: true })
+                        ),
+                        React.createElement(
+                            "div",
+                            { "class": "clearfix" },
+                            React.createElement(
+                                "button",
+                                { type: "submit", "class": "btn btn-primary", "data-toggle": "modal", "data-target": "#exampleModal" },
+                                "\u5831\u5230"
+                            )
+                        )
+                    )
+                )
+            );
+        }
+    }]);
+
+    return RollCallForm;
 }(React.Component);
 
-ReactDOM.render(React.createElement(RollCall, null), document.getElementById('root'));
+var Message = function (_React$Component2) {
+    _inherits(Message, _React$Component2);
+
+    function Message() {
+        _classCallCheck(this, Message);
+
+        return _possibleConstructorReturn(this, (Message.__proto__ || Object.getPrototypeOf(Message)).apply(this, arguments));
+    }
+
+    _createClass(Message, [{
+        key: "render",
+        value: function render() {
+            return React.createElement(
+                "div",
+                { "class": "modal fade", id: "exampleModal", tabindex: "-1", role: "dialog", "aria-labelledby": "exampleModalLabel", "aria-hidden": "true" },
+                React.createElement(
+                    "div",
+                    { "class": "modal-dialog", role: "document" },
+                    React.createElement(
+                        "div",
+                        { "class": "modal-content" },
+                        React.createElement(
+                            "div",
+                            { "class": "modal-header" },
+                            React.createElement(
+                                "h5",
+                                { "class": "modal-title", id: "exampleModalLabel" },
+                                "\u5831\u5230\u6210\u529F"
+                            ),
+                            React.createElement(
+                                "button",
+                                { type: "button", "class": "close", "data-dismiss": "modal", "aria-label": "Close" },
+                                React.createElement(
+                                    "span",
+                                    { "aria-hidden": "true" },
+                                    "\xD7"
+                                )
+                            )
+                        ),
+                        React.createElement(
+                            "div",
+                            { "class": "modal-body" },
+                            "Jimmy \u5831\u5230\u6210\u529F"
+                        ),
+                        React.createElement(
+                            "div",
+                            { "class": "modal-footer" },
+                            React.createElement(
+                                "button",
+                                { type: "button", "class": "btn btn-primary", "data-dismiss": "modal" },
+                                "\u95DC\u9589"
+                            )
+                        )
+                    )
+                )
+            );
+        }
+    }]);
+
+    return Message;
+}(React.Component);
+
+var App = function (_React$Component3) {
+    _inherits(App, _React$Component3);
+
+    function App() {
+        _classCallCheck(this, App);
+
+        return _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).apply(this, arguments));
+    }
+
+    _createClass(App, [{
+        key: "render",
+        value: function render() {
+            return React.createElement(
+                "div",
+                null,
+                React.createElement(RollCallForm, null),
+                React.createElement(Message, null)
+            );
+        }
+    }]);
+
+    return App;
+}(React.Component);
+
+ReactDOM.render(React.createElement(App, null), document.getElementById('root'));
--- src/index.js
+++ src/index.js
@@ -1,23 +1,73 @@
 'use strict';
 
-class RollCall extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { liked: false };
+class RollCallForm extends React.Component {  
+    render() {  
+      return (
+            <div>
+                <form action="#">
+                    <div class="container">
+                    <h1>Roll Call</h1>
+                    <div>是否有信徒編號: <input type="checkbox" name="check_member" /></div>
+
+                    <div>
+                        <label for="member_id"><b>編號</b></label>
+                        <input type="text" placeholder="請輸入" name="member_id" required />      
+                    </div>
+                    <div>
+                        <label for="name"><b>姓名</b></label>
+                        <input type="text" placeholder="請輸入" name="name" required />      
+                    </div>
+                    <div>
+                        <label for="phone"><b>電話號碼</b></label>
+                        <input type="text" placeholder="請輸入" name="phone" required />      
+                    </div>
+                    <div class="clearfix">
+                        <button type="submit" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">報到</button>
+                    </div>
+                    </div>
+                </form>
+            </div>
+      );
+    }
   }
 
-  render() {
-    if (this.state.liked) {
-      return 'You liked this.';
+  
+class Message extends React.Component {  
+    render() {  
+      return (
+        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">報到成功</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                Jimmy 報到成功
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary" data-dismiss="modal">關閉</button>
+            </div>
+            </div>
+        </div>
+        </div>
+      );
     }
+  }
+  
 
+class App extends React.Component {
+  render() {
     return (
-        <button onClick={() => this.setState({ liked: true })}>
-        Like
-      </button>
+        <div>
+            <RollCallForm />
+            <Message />
+        </div>
     );
   }
 }
 
 
-ReactDOM.render(<RollCall />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
