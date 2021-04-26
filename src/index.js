1aca1544654a72d75b325387eb8881cae46ea695
JimmyMa <jimmatw@gmail.com>
2021-04-26 18:02:30 +0800

    add note

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-let myFunction = () => {
+$(() => {
+  let myFunction = () => {
     var x = document.getElementById("myDIV");
     if (x.style.display === "none") {
       x.style.display = "block";
@@ -6,3 +7,31 @@ let myFunction = () => {
       x.style.display = "none";
     }
   }
+
+  // 登入腳本
+  $('#rollcallForm').submit((e) => {
+      e.preventDefault();
+      let member_id = $("#member_id input").val();
+      let name = $("#name input").val();
+      let phone = $("#phone input").val();
+      $.ajax({
+          type:"POST",
+          url:"/this_url/",
+          data: {
+            member_id: member_id,
+            name: name,
+            phone: phone,
+          },
+          success: () => {
+            alert('success')
+          },
+          error: () => {
+            alert('error')
+          },
+          complete: () => {
+            alert('complete')
+        },
+      });
+  });
+
+});
