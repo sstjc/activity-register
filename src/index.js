b82a19ecf24bc296078c5ffe54f6c58de0ff81e4
JimmyMa <jimmatw@gmail.com>
2021-04-28 10:17:56 +0800

    init submit function for normal js

--- src/index.js
+++ src/index.js
@@ -10,28 +10,30 @@ $(() => {
 
   // 登入腳本
   $('#rollcallForm').submit((e) => {
-      e.preventDefault();
-      let member_id = $("#member_id input").val();
-      let name = $("#name input").val();
-      let phone = $("#phone input").val();
-      $.ajax({
-          type:"POST",
-          url:"/this_url/",
-          data: {
-            member_id: member_id,
-            name: name,
-            phone: phone,
-          },
-          success: () => {
-            alert('success')
-          },
-          error: () => {
-            alert('error')
-          },
-          complete: () => {
-            alert('complete')
+    e.preventDefault();
+
+    let member_id = $("#member_id input").val();
+    let name = $("#name input").val();
+    let phone = $("#phone input").val();
+    $.ajax({
+        type:"POST",
+        url: "http://localhost:5000/rollcall",
+        data: {
+          id: member_id,
+          name: name,
+          phone: phone,
         },
-      });
+        success: (res_data) => {
+          alert(res_data)
+          // alert('success')
+        },
+        error: (res_data) => {
+          alert('error')
+        },
+        complete: (res_data) => {
+          alert('complete')
+      },
+    });
   });
 
 });
