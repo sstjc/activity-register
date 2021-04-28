d6b302c7ad4ec94d272cf10f53f9abe2a295548a
JimmyMa <jimmatw@gmail.com>
2021-04-28 17:33:13 +0800

    init finish function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 $(() => {
+  let clean_data = () => {
+    $("#member_id input").val('')
+    $("#name input").val('')
+    $("#phone input").val('')
+  }
+
   $("input[type='checkbox']").click(() => {
     $.each($("form#rollcallForm div.input-list div"), (index, value) => {
+      // toggle hidden_form
       value.classList.toggle("hidden_form");
+      
+      // toggle required
+      let this_input = value.children[1]
+      this_input.required = ! this_input.required
+
+      clean_data()
     })
   })
 
@@ -14,14 +27,16 @@ $(() => {
     let phone = $("#phone input").val();
     $.ajax({
         type:"POST",
-        url: "http://localhost:5000/rollcall",
+        // url: "http://localhost:5000/rollcall",
+        url: "https://ngghkzcdl5.execute-api.us-east-1.amazonaws.com/dev/rollcall/",
         data: {
           id: member_id,
-          name: name,
-          phone: phone,
+          vistor_name: name,
+          vistor_phone: phone,
         },
         success: (res_data) => {
           alert(res_data)
+          clean_data()
         },
         error: (res_data) => {
           alert('未知錯誤，報到失敗')
